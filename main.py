3d8b7806d453a2a742c40ddbf2d997e83bf29a22
AhmadBouMerhi <39493645+AhmadBouMerhi@users.noreply.github.com>
2018-08-18 11:27:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -18,17 +18,17 @@ with open('election_data.csv', mode='r') as csv_file:
 
 
 # takes the unique number of candidates and stores them as an alphabetically ordered list
-#
+
 unique_candidates = list(sorted(set(candidates)))
 
 # vote_count is the vote for each unique candidate and these values will be stored in a list
-#
+
 vote_count = 0
 unique_candidate_votes = []
 
 # for every unique candidate, this block of code goes down the list of candidates and tallys up the
 # votes for each unique candidate and appends them to a list. 
-# 
+ 
 for i in range(len(unique_candidates)):
     for candidate in candidates:
         if candidate == unique_candidates[i]:
@@ -37,15 +37,15 @@ for i in range(len(unique_candidates)):
     vote_count = 0
 
 # calculates the percent of votes each unique candidate got and adds it to a list
-#
+
 percent_votes = [round((x/total_votes)*100, 2) for x in unique_candidate_votes]
 
 # zips up the data for each unique candidate and stores it in a list that is in descending order from most votes to least votes
-#
+
 results = sorted(list(zip(unique_candidates, unique_candidate_votes, percent_votes)),  key=lambda vote: vote[1], reverse = True)
 
 # since results list in descending order by name, the first name in the list will be the winner
-#
+
 for a, b, c in results:
     winner = a
     break
@@ -75,14 +75,14 @@ with open('output.txt', 'w', newline='') as textfile:
 
 
 
-##########################################################################
-# 
+########################
+
 # Disregard.... this was the hard-coded solution
-#
+
 # since the unique_candidates list was sorted, we know that the nth element in unique_candidate_votes corresponds
 # to the nth element in unique_candidates.
 # this allows us to create a dictionary out of these pairs
-# 
+
 # voting_dict = dict(zip(unique_candidates,unique_candidate_votes))
         
 # correy_votes = 0
